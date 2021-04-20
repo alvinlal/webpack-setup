5518af16c92e7aac785d2fd1bb24c8268a58ff32
alvinlal <alvinzzz2001@gmail.com>
2021-04-21 01:46:50 +0530

    cache busting and htmlWebPackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development", // build mode, can be production or development
   entry: "./src/index.js", // the entry point of our app
   output: {
-    filename: "main.js", // filename of output file
+    filename: "main.[contenthash].js", // filename of output file ,
+    //contenthash will be replaced by a unique hash each time the file is updated, this is for cache busting.
+    //you have to use html-webpack-plugin, for generating the html file with correct script tag.
     path: path.resolve(__dirname, "dist"), // output directory name and path
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ], // generates index.html with correct script tags using a template
   module: {
     // specify loaders
     rules: [
